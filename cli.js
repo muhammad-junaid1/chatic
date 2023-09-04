78edcb34cd78cbc8dd81ce6ce142f426bf1f57c9
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-09-04 23:27:35 +0500

    random room connections

--- cli.js
+++ cli.js
@@ -8,41 +8,47 @@ import chalk from "chalk";
 const backendURL = "http://localhost:5000";
 
 (() => {
- const socket = io(backendURL);
- inquirer.prompt([
-    {
-        type: "input", 
-        name: "username", 
-        message: "Enter your username: "
-    }
- ]).then((result) => {
-     const username = result?.username;
-     
-     socket.emit("chatic_add-user", username);
-     socket.on("chatic_user-added", (data) => {
-         if(data?.status) {
-            const spinner = ora("Connecting you to our servers!").start();
-            setTimeout(() =>{
-                spinner.stop();
-                
-                console.log(`Boom! You're connected as ${chalk.greenBright(username)}`);
+  const socket = io(backendURL);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "username",
+        message: "Enter your username: ",
+      },
+    ])
+    .then((result) => {
+      const username = result?.username;
 
-                inquirer.prompt([
-                    {
-                        type: "list", 
-                        name: "Options", 
-                        choices: [
-                            "1. One-to-One Random Chat"
-                        ]
-                    }
-                ]).then((result) => {
-                    const choice = result?.Options;
-                    console.log(data?.users);
-                })
-            }, 2000);
-        } else {
-            console.log(chalk.redBright(data?.message));
-        }
-    })
- })
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "Options",
+            choices: ["1. One-to-One Random Chat"],
+          },
+        ])
+        .then((result) => {
+          const choice = result?.Options;
+          socket.emit("chatic_add-user", username);
+          socket.on("chatic_user-added", (data) => {
+            if (data?.status) {
+              const spinner = ora("Connecting you to our servers!").start();
+              setTimeout(() => {
+                spinner.stop();
+                const { user1, user2 } = data?.room;
+
+                const connectedTo =
+                  user1?.username === username
+                    ? user2?.username
+                    : user1?.username;
+                console.log("\n");
+                console.log(`Connected to: ${chalk.blueBright(connectedTo)}`);
+              }, 2000);
+            } else {
+              console.log(chalk.redBright(data?.message));
+            }
+          });
+        });
+    });
 })();
